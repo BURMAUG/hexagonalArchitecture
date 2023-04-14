d2dea82d6893c9ef0965fd45f48ed09e22488082
Burmau Garba <burmaud1@gmail.com>
2023-04-13 23:36:40 -0400

    initial commit

---
+++ src/main/java/framework/adapters/input/stdin/RouterViewCLIAdapter.java
@@ -0,0 +1,4 @@
+package framework.adapters.input.stdin;
+
+public class RouterViewCLIAdapter {
+}
---
+++ src/main/java/framework/adapters/output/file/RouterViewFileAdapter.java
@@ -0,0 +1,5 @@
+package framework.adapters.output.file;
+
+public class RouterViewFileAdapter {
+
+}
